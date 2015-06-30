1bb0e4b9b31376e7a23e5c6ea86634ebcc918844
Jan Lukas Deichmann <github@pezam.de>
2015-06-30 02:09:19 +0200

    change vibrance display

--- csgo/vibrancetoggle.sh
+++ csgo/vibrancetoggle.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 VAL=500
-if [[ $(nvidia-settings -q "[gpu:0]/DigitalVibrance[DFP-1]" | grep "Attribute.*$VAL\.") ]]
+if [[ $(nvidia-settings -q "[gpu:0]/DigitalVibrance[DFP-0]" | grep "Attribute.*$VAL\.") ]]
 then
-	nvidia-settings -a "[gpu:0]/DigitalVibrance[DFP-1]=0" > /dev/null
+	nvidia-settings -a "[gpu:0]/DigitalVibrance[DFP-0]=0" > /dev/null
 	echo "Vibrance Disabled"
 else
-	nvidia-settings -a "[gpu:0]/DigitalVibrance[DFP-1]=$VAL" > /dev/null
+	nvidia-settings -a "[gpu:0]/DigitalVibrance[DFP-0]=$VAL" > /dev/null
 	echo "Vibrance Enabled"
 fi
