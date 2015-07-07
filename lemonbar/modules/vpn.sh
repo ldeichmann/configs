9649a5ab9adc50ce26f1a2b4daac51248e2e7ac3
Jan Lukas Deichmann <github@pezam.de>
2015-07-07 18:06:56 +0200

    same here

--- lemonbar/modules/vpn.sh
+++ lemonbar/modules/vpn.sh
@@ -1,7 +1,8 @@
+CLICK="%{A1:urxvt -e nmtui &:}"
 ps -aux | grep openvpn | grep -v 'grep' | grep -v 'sed' > /dev/null
 if [ $? -eq 1 ]; then
-	echo -n "%{F#ffff6961}""""%{F#ffffffff}"
+	echo -n $CLICK"%{F#ffff6961}""""%{F#ffffffff}""%{A}"
 else
 	VPN=$(ps -aux | grep openvpn | grep -v 'grep openvpn' | egrep -o '\-\-remote [a-z\.]+' | sed 's/--remote //g')
-	echo -n " $VPN"
+	echo -n $CLICK" $VPN""%{A}"
 fi
