d9a2d4303f9913991333f8e4f6bbc703155c4988
Jan Lukas Deichmann <github@pezam.de>
2016-03-28 13:55:20 +0200

    change lockscreen filter

--- i3/lock.sh
+++ i3/lock.sh
@@ -15,7 +15,9 @@ SCREENSHOT="scrot $IMAGE" # 0.46s
 
 # Get the screenshot, add the blur and lock the screen with it
 $SCREENSHOT
-convert -scale 10% -scale 1000% $IMAGE $IMAGE
+#convert -scale 10% -scale 1000% $IMAGE $IMAGE
+convert -filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500.5% -set colorspace Gray -separate -average $IMAGE $IMAGE
+sleep 5
 i3lock -i $IMAGE
 sleep 5
 rm $IMAGE
